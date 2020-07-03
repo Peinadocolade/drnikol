1f2404fef46fd930bb1a9feed357e5d9d20a4f4f
sheggy777 <xxx.jaycpi@gmail.com>
2020-07-03 14:17:03 +0400

    1

--- price.js
+++ price.js
@@ -46,16 +46,14 @@ $(function() {
 
     selects = $("select[name='country']");
     selects.append('<option value="RU">Россия</option>');
-
-    selects.append('<option value="KZ" ' + kz_selected + '>Казахстан</option>');
-    //  selects.append('<option value="UA" ' + ua_selected + '>Украина</option>');
-    //selects.append('<option value="BY" ' + by_selected + '>Белоруссия</option>');
-
-    // selects.append('<option value="MD" ' + md_selected + '>Молдова</option>');
+    // selects.append('<option value="KZ" ' + kz_selected + '>Казахстан</option>');
+    // selects.append('<option value="BY" ' + by_selected + '>Беларусь</option>');
     // selects.append('<option value="GE" ' + ge_selected + '>Грузия</option>');
-    //   selects.append('<option value="AM" ' + am_selected + '>Армения</option>');
+    selects.append('<option value="UA" ' + ua_selected + '>Украина</option>');
+    // selects.append('<option value="MD" ' + md_selected + '>Молдова</option>');
+    // selects.append('<option value="AM" ' + am_selected + '>Армения</option>');
     // selects.append('<option value="AZ" ' + az_selected + '>Азербайджан</option>');
-    // selects.append('<option value="KG" ' + kg_selected + '>Кыргызстан</option>');
+    //selects.append('<option value="KG" ' + kg_selected + '>Кыргызстан</option>');
 
     var change = 0,
         updatePrices = function(item) {
@@ -70,54 +68,58 @@ $(function() {
             }
 
             if (sel == 'RU') {
-                $('.old_price_val').html('5980');
+                $('.old_price_val').html('2275');
                 $('.old_price_cur').html('руб');
-                $('.old_price_sig').html('&#8381;');
+                $('.old_price_sig').html('руб');
                 $('.new_price_val').html('149');
                 $('.new_price_cur').html('руб*');
                 $('.new_price_sig').html('руб');
                 $('select').val('RU').trigger('change');
-
+                initializeMask('remove')
             }
 
             if (sel == 'KZ') {
-                $('.old_price_val').html('10580');
+                $('.old_price_val').html('18900');
                 $('.old_price_cur').html('тнг');
                 $('.old_price_sig').html('тнг*');
                 $('.new_price_val').html('830');
                 $('.new_price_cur').html('тнг*');
                 $('.new_price_sig').html('тнг');
                 $('select').val('KZ').trigger('change');
+                initializeMask('remove')
             }
 
             if (sel == 'UA') {
-                $('.old_price_val').html('830');
-                $('.old_price_cur').html('грн');
+                $('.old_price_val').html('670');
+                $('.old_price_cur').html('грн*');
                 $('.old_price_sig').html('грн*');
                 $('.new_price_val').html('69');
                 $('.new_price_cur').html('грн*');
                 $('.new_price_sig').html('грн');
                 $('select').val('UA').trigger('change');
+                initializeMask({ mask: "(+38)9999999999", removeMaskOnSubmit: true })
             }
 
             if (sel == 'MD') {
                 $('.old_price_val').html('9');
-                $('.old_price_cur').html('лей');
+                $('.old_price_cur').html('лей*');
                 $('.old_price_sig').html('лей*');
                 $('.new_price_val').html('9');
                 $('.new_price_cur').html('лей*');
                 $('.new_price_sig').html('лей');
                 $('select').val(sel).trigger('change');
+                initializeMask('remove')
             }
 
             if (sel == 'GE') {
                 $('.old_price_val').html('90');
                 $('.old_price_cur').html('gel');
                 $('.old_price_sig').html('gel*');
-                $('.new_price_val').html('39');
+                $('.new_price_val').html('15');
                 $('.new_price_cur').html('gel*');
                 $('.new_price_sig').html('gel');
                 $('select').val(sel).trigger('change');
+                initializeMask('remove')
             }
 
             if (sel == 'BY') {
@@ -128,36 +130,40 @@ $(function() {
                 $('.new_price_cur').html('р*');
                 $('.new_price_sig').html('р');
                 $('select').val(sel).trigger('change');
+                initializeMask('remove')
             }
 
             if (sel == 'AM') {
-                $('.old_price_val').html('25000');
+                $('.old_price_val').html('990');
                 $('.old_price_cur').html('др');
-                $('.old_price_sig').html('др*');
-                $('.new_price_val').html('12890');
+                $('.old_price_sig').html('др');
+                $('.new_price_val').html('1');
                 $('.new_price_cur').html('др*');
                 $('.new_price_sig').html('др');
                 $('select').val(sel).trigger('change');
+                initializeMask({ mask: "(+374)99999999[9]", removeMaskOnSubmit: true })
             }
 
             if (sel == 'AZ') {
                 $('.old_price_val').html('69');
-                $('.old_price_cur').html('манат');
+                $('.old_price_cur').html('манат*');
                 $('.old_price_sig').html('манат*');
                 $('.new_price_val').html('29');
                 $('.new_price_cur').html('манат*');
                 $('.new_price_sig').html('манат');
                 $('select').val(sel).trigger('change');
+                initializeMask('remove')
             }
 
             if (sel == 'KG') {
-                $('.old_price_val').html('2758');
-                $('.old_price_cur').html('сом');
+                $('.old_price_val').html('3380');
+                $('.old_price_cur').html('сом*');
                 $('.old_price_sig').html('сом*');
-                $('.new_price_val').html('1099');
+                $('.new_price_val').html('1690');
                 $('.new_price_cur').html('сом*');
                 $('.new_price_sig').html('сом');
                 $('select').val(sel).trigger('change');
+                initializeMask({ mask: "(+\\9\\96)999999999[9]", removeMaskOnSubmit: true })
             }
 
             change = 0;
@@ -166,4 +172,9 @@ $(function() {
 
         if (change == 0) updatePrices(this);
     }).change();
+
+    function initializeMask(mask) {
+        $('[name=phone]').inputmask(mask);
+    }
+
 });
